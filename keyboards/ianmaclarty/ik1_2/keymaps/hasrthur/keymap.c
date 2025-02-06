938d025032f33b7e5347a00fad863b766f098273
Artur Borysov <arthur.borisow@gmail.com>
2025-02-07 00:37:44 +0200

    Update keymap

--- keyboards/ianmaclarty/ik1_2/keymaps/hasrthur/keymap.c
+++ keyboards/ianmaclarty/ik1_2/keymaps/hasrthur/keymap.c
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [LBASE1] = LAYOUT_split_4x6_3(
 _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                              KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
-KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                              KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
-KC_LALT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                              KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                              KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RGUI,
-                                                KC_LCTL,    TABNUM,     KC_SPC,    BSSFT,      NAV,        ENTFN
+KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                              KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_EQL,
+KC_TAB,    MT(MOD_LCTL, KC_A),     MT(MOD_LALT, KC_S),       MT(MOD_LGUI, KC_D),       MT(MOD_LSFT, KC_F),       KC_G,                              KC_H,       MT(MOD_LSFT, KC_J),       MT(MOD_LGUI, KC_K),       MT(MOD_LALT, KC_L),       MT(MOD_LCTL, KC_SCLN),    KC_QUOT,
+KC_BSLS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                              KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_MINS,
+                                                MOD_HYPR,   KC_ENT,     KC_SPC,    KC_BSPC,    OSM(MOD_LSFT),    KC_GRV
 ),
 
 [LBASE2] = LAYOUT_split_4x6_3(
